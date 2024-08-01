8cf0cd42097bd4209f9fb7332cb42aa548a5cd37
Matheus <mt121207@gmail.com>
2024-08-01 18:39:58 -0300

    Adicionando as duas primeiras aulas

---
+++ variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Matheus"
+idade = 29
+
+nome, idade = "Teste", 28
+
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
