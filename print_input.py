cc1164c7d2a923e41ae3b03d926299f86e50082f
Matheus <mt121207@gmail.com>
2024-08-06 12:39:36 -0300

    aulas básicas de python

---
+++ print_input.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome: ")
+print(nome)
+
+nome = "Matheus"
+sobrenome = "Silva"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+
