cb9a1b04feaeb2f58ea0fd8e29a626b6c2026dc9
Matheus <mt121207@gmail.com>
2024-08-06 13:18:31 -0300

    aulas sobre estruturas de repetição

---
+++ estrutura_repeticao_for.py
@@ -0,0 +1,13 @@
+texto = input("informe um texto: ")
+VOGAIS = "AEIOU"
+
+# Exemplo utilizando um iterável.
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+
+print()
+
+# Exemplo utilizando a função built-in range.
+for numero in range(0, 51, 5):
+    print(numero, end=" ") 
